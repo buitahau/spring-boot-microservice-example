a3edd9e98f2772bbd3b1eca683255defac647b8d
buitahau@gmail.com <buitahau@gmail.com>
2021-01-01 07:08:54 +0700

    Internationalized message

--- spring-boot-28mins-exercise/restful-webservice-with-spring-boot/src/main/java/hau/kute/spring/tutorial/springbootmicroservice/SpringBootMicroserviceApplication.java
+++ spring-boot-28mins-exercise/restful-webservice-with-spring-boot/src/main/java/hau/kute/spring/tutorial/springbootmicroservice/SpringBootMicroserviceApplication.java
@@ -2,12 +2,33 @@ package hau.kute.spring.tutorial.springbootmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class SpringBootMicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMicroserviceApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource bundleMessageSource() {
+		ResourceBundleMessageSource resourceBundleMessageSource = new
+						ResourceBundleMessageSource();
+		resourceBundleMessageSource.setBasename("messages");
+		return resourceBundleMessageSource;
 	}
 
 }
