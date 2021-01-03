2e74d24c5910789402fdb170999268db7fce8c32
buitahau@gmail.com <buitahau@gmail.com>
2021-01-04 06:20:37 +0700

    Access user api via eureka discovery service

--- spring-boot-28mins-exercise/restful-webservice-with-spring-boot/src/main/java/hau/kute/spring/tutorial/springbootmicroservice/SpringBootMicroserviceApplication.java
+++ spring-boot-28mins-exercise/restful-webservice-with-spring-boot/src/main/java/hau/kute/spring/tutorial/springbootmicroservice/SpringBootMicroserviceApplication.java
@@ -2,6 +2,7 @@ package hau.kute.spring.tutorial.springbootmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import java.util.Locale;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringBootMicroserviceApplication {
 
 	public static void main(String[] args) {
